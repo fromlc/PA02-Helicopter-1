3370f4d60448e68258124bc39b68c93a7d5cd353
L Carver <wifihum@gmail.com>
2026-03-03 15:37:07 -0800

    Helicopter::goLand() does not change distance flown now.

--- source/Helicopter.cpp
+++ source/Helicopter.cpp
@@ -62,6 +62,6 @@ int Helicopter::goForward(int incDistance)
 }
 
 //------------------------------------------------------------------------------
-// resets altitude and forward distance to 0
+// resets altitude to 0
 //------------------------------------------------------------------------------
-void Helicopter::goLand() { altitude = distance = 0; }
+void Helicopter::goLand() { altitude = 0; }
